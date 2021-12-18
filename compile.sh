f6be5e66e2bc1ee99331f1a9df22dc01163c442e
Thibaud Tonnellier <23101698+ttonnellier@users.noreply.github.com>
2021-12-18 23:32:24 +0100

    better compile script.

--- compile.sh
+++ compile.sh
@@ -1,1 +1,2 @@
-g++ $1 -Wall -Wextra -Wcast-align -Wunused -Wpedantic -Wformat=2 -Wduplicated-cond -Wduplicated-branches -Wuseless-cast -Wundef -Wlogical-op -Wmissing-field-initializers -O3
+out=$(basename -s .cpp $1)
+g++ $1 -Wall -Wextra -Wcast-align -Wunused -Wpedantic -Wformat=2 -Wduplicated-cond -Wduplicated-branches -Wuseless-cast -Wundef -Wlogical-op -Wmissing-field-initializers -O3 -o $out
