fe4ff291fbf0e568118c94589ce475a124dc2a58
Thibaud Tonnellier <23101698+ttonnellier@users.noreply.github.com>
2021-12-22 14:16:59 +0100

    Clean in Day 8

--- day_08.cpp
+++ day_08.cpp
@@ -10,10 +10,32 @@
 #include <cassert>
 #include <bitset>
 
+constexpr int PATTERNS = 10;
+constexpr int OUTPUTS  = 4;
+constexpr int LINES    = 1000;
+
+void read_line(std::string& observation, std::vector<uint8_t>&  result, const int size)
+{
+    int idx = 0;
+    size_t separator;
+
+    while ( (separator = observation.find(" ")) !=  std::string::npos )
+    {
+        std::string segments_on = observation.substr(0, separator);
+        observation.erase(0, separator+1);
+
+        for ( const auto c: segments_on )
+            result[idx] += ( 1 << (c - 'a') ); // c ranges up to g, we therefore shift up to 6
+        ++idx;
+    }
+    for ( const auto c: observation ) // observation contains the last 7-segments observation
+        result[idx] += ( 1 << (c - 'a') );
+    assert (idx == size-1);
+}
+
 std::tuple<std::vector<std::vector<uint8_t>>, std::vector<std::vector<uint8_t>>> read_file(const std::string& filename)
 {
-    constexpr int PATTERNS = 10;
-    constexpr int OUTPUTS  = 4;
+
     std::ifstream file(filename);
     if ( !file )
     {
@@ -23,6 +45,9 @@ std::tuple<std::vector<std::vector<uint8_t>>, std::vector<std::vector<uint8_t>>>
 
     std::vector<std::vector<uint8_t>> patterns(0, std::vector<uint8_t>(PATTERNS, 0));
     std::vector<std::vector<uint8_t>>  outputs(0, std::vector<uint8_t>(OUTPUTS , 0));
+
+    patterns.reserve(LINES);
+    outputs.reserve(LINES);
 
     std::string line;
     while ( std::getline(file, line) )
@@ -34,33 +59,8 @@ std::tuple<std::vector<std::vector<uint8_t>>, std::vector<std::vector<uint8_t>>>
         std::string pattern_s = line.substr(0, separator);
         std::string output_s  = line.substr(separator+3, line.length());
 
-        int idx = 0;
-        while ( (separator = pattern_s.find(" ")) !=  std::string::npos )
-        {
-            std::string elem = pattern_s.substr(0, separator);
-            pattern_s.erase(0, separator+1);
-
-            for ( auto c: elem )
-                patterns.back()[idx] += (1 << (static_cast<uint8_t>(c)-0x61)); // c ranges up to g, we therefore shift up to 6
-            ++idx;
-        }
-        for ( auto c: pattern_s )
-            patterns.back()[idx] += (1 << (static_cast<uint8_t>(c)-0x61));
-        assert (idx == PATTERNS-1);
-
-        idx = 0;
-        while ( (separator = output_s.find(" ")) !=  std::string::npos )
-        {
-            std::string elem = output_s.substr(0, separator);
-            output_s.erase(0, separator+1);
-
-            for ( auto c: elem )
-                outputs.back()[idx] += (1 << (static_cast<uint8_t>(c)-0x61));
-            ++idx;
-        }
-        for ( auto c: output_s )
-            outputs.back()[idx] += (1 << (static_cast<uint8_t>(c)-0x61));
-        assert (idx == OUTPUTS-1);
+        read_line(pattern_s, patterns.back(), PATTERNS);
+        read_line(output_s , outputs.back() , OUTPUTS);
     }
 
     file.close();
@@ -75,49 +75,49 @@ std::map<uint8_t, uint8_t> pattern_analysis(std::vector<uint8_t>& pattern)
 
     std::map<uint8_t, uint8_t> numbers;
 
-    auto it = std::find_if(pattern.begin(), pattern.end(), [](uint8_t i){ return __builtin_popcount(i) == 2; });
-    auto one = *it;
+    auto it      = std::find_if(pattern.begin(), pattern.end(), [](uint8_t i){ return __builtin_popcount(i) == 2; });
+    auto one     = *it;
     numbers[one] = 1;
     pattern.erase(it);
 
-    it = std::find_if(pattern.begin(), pattern.end(), [](uint8_t i){ return __builtin_popcount(i) == 4; });
-    auto four = *it;
+    it            = std::find_if(pattern.begin(), pattern.end(), [](uint8_t i){ return __builtin_popcount(i) == 4; });
+    auto four     = *it;
     numbers[four] = 4;
     pattern.erase(it);
 
-    it = std::find_if(pattern.begin(), pattern.end(), [](uint8_t i){ return __builtin_popcount(i) == 3; });
-    auto seven = *it;
+    it             = std::find_if(pattern.begin(), pattern.end(), [](uint8_t i){ return __builtin_popcount(i) == 3; });
+    auto seven     = *it;
     numbers[seven] = 7;
     pattern.erase(it);
 
-    it = std::find_if(pattern.begin(), pattern.end(), [](uint8_t i){ return __builtin_popcount(i) == 7; });
-    auto eight = *it;
+    it             = std::find_if(pattern.begin(), pattern.end(), [](uint8_t i){ return __builtin_popcount(i) == 7; });
+    auto eight     = *it;
     numbers[eight] = 8;
     pattern.erase(it);
 
     it = std::find_if(pattern.begin(), pattern.end(), [](uint8_t i){ return __builtin_popcount(i) == 6; });
     while (((*it) & one) == one)
         it = std::find_if(it+1, pattern.end(), [](uint8_t i){ return __builtin_popcount(i) == 6; });
-    auto six = *it;
+    auto six     = *it;
     numbers[six] = 6;
     pattern.erase(it);
 
     it = std::find_if(pattern.begin(), pattern.end(), [](uint8_t i){ return __builtin_popcount(i) == 6; });
     while ((*it & four) != four)
         it = std::find_if(it+1, pattern.end(), [](uint8_t i){ return __builtin_popcount(i) == 6; });
-    auto nine = *it;
+    auto nine     = *it;
     numbers[nine] = 9;
     pattern.erase(it);
 
-    it = std::find_if(pattern.begin(), pattern.end(), [](uint8_t i){ return __builtin_popcount(i) == 6; });
-    auto zero = *it;
+    it            = std::find_if(pattern.begin(), pattern.end(), [](uint8_t i){ return __builtin_popcount(i) == 6; });
+    auto zero     = *it;
     numbers[zero] = 0;
     pattern.erase(it);
 
     it = std::find_if(pattern.begin(), pattern.end(), [](uint8_t i){ return __builtin_popcount(i) == 5; });
     while ((*it & seven) != seven)
         it = std::find_if(it+1, pattern.end(), [](uint8_t i){ return __builtin_popcount(i) == 5; });
-    auto three = *it;
+    auto three     = *it;
     numbers[three] = 3;
     pattern.erase(it);
 
@@ -135,13 +135,13 @@ std::map<uint8_t, uint8_t> pattern_analysis(std::vector<uint8_t>& pattern)
 int main()
 {
 
-    auto [patterns, outputs] = read_file("day_08.txt");
+    const auto [patterns, outputs] = read_file("day_08.txt");
 
     auto count = 0;
     for (const auto& line: outputs)
         for (const auto& number: line)
         {
-            auto pcount = __builtin_popcount(number);
+            const auto pcount = __builtin_popcount(number);
             if (pcount == 2 || pcount == 4 || pcount == 3 || pcount == 7)
                 ++count;
         }
@@ -151,13 +151,15 @@ int main()
     uint64_t sum = 0;
     for (auto line = 0u; line < patterns.size(); line++)
     {
-        auto pattern = patterns[line];
+        auto pattern       = patterns[line];
         const auto numbers = pattern_analysis(pattern);
 
-        auto output = outputs[line];
+        const auto output  = outputs[line];
+
         auto value = 0;
-        for (auto i: output)
+        for (const auto& i: output)
             value = value*10 + numbers.at(i);
+
         sum += value;
     }
     std::cout << "Part2: " << sum << std::endl;
